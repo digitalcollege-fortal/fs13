0a95e982ab83a5d5a86dd3b38355410c899e10e6
Alessandro Feitoza <06114387392@DIGITALCOLLEGE.LOCAL>
2023-09-19 23:08:05 -0300

    Aula 10

--- fs13-backend/aula09/src/routes/category.js
+++ fs13-backend/aula09/src/routes/category.js
@@ -15,15 +15,42 @@ router.get('/categorias/:id', async (req, res) => {
     res.send(data);
 });
 
-// router.post('/categorias', async (req, res) => {
+router.post('/categorias', async (req, res) => {
+    let data = await Category.create(req.body); 
 
-//     let data = await Category.create({
-//         name: req.body.name,
-//         description: req.body.description,
-//     });
+    res.send(data);
+});
 
-//     res.send(data);
-// });
+router.delete('/categorias/:id', async (req, res) => {
+    let linhasExcluidas = await Category.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    let status = linhasExcluidas === 0 ? 404 : 204;
+    res.status(status).end();
+    
+    // let cat = await Category.findByPk(req.params.id);
+    // cat.destroy();
+});
+
+router.patch('/categorias/:id', async (req, res) => {
+    // let cat = await Category.findByPk(req.params.id);
+
+    // cat.name = req.body.name;
+    // cat.description = req.body.description;
+    // cat.save();
+
+    // res.send(cat);
+
+    await Category.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.send(req.body);
+});
 
 
 module.exports = router;
