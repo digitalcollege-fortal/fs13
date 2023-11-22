76c7d4de99be4d834e4fe5cc159dfa6370a9e325
Alessandro Feitoza <06114387392@DIGITALCOLLEGE.LOCAL>
2023-11-21 21:54:05 -0300

    Aula 02 frontend parte 2

--- aulatestes-02/app/cypress/e2e/home.cy.js
+++ aulatestes-02/app/cypress/e2e/home.cy.js
@@ -2,10 +2,23 @@ describe('Teste da pagina inicial', () => {
     it ('testando que a pagina funciona no desktop', () => {
         cy.visit('http://localhost:3000');
 
-        cy.contains('HOME').should('visible');
+        cy.contains('HOME').should('exist');
+
+        // garantir que todo o menu esta visivel (item por item)
+        // garantir que o input de pesquisar existe
+        // garantir que o botao de pesquisar existe
+        // garantir que a logo esta visivel
+        // garantir que a navbar está na cor #666
+        // garantir que o texto do menu é branco
+
+        cy.get('.menu a').should('have.css', 'color', 'rgb(255, 255, 255)');
+        cy.get('.container').should('have.css', 'background-color', 'rgb(102, 102, 102)');
+
+
+        cy.get('.menu-mobile').should('not.exist');
     });
 
-    it ('testando que quando eu acesso usando o celular nao aparece o HOME', () => {
+    it ('testando que quando eu acesso usando o celular', () => {
         cy.visit('http://localhost:3000');
 
         cy.viewport('iphone-6');
